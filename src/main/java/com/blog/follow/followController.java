1feefb300279fa5845368e7579916fbc122c4c57
DESKTOP-5P9OAHR\user <test@test.com>
2024-08-21 18:08:07 +0900

    springProject_240821

--- src/main/java/com/blog/follow/followController.java
+++ src/main/java/com/blog/follow/followController.java
@@ -1,14 +1,34 @@
 package com.blog.follow;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.blog.blog.bo.BlogBO;
+import com.blog.blog.domain.CardView;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/follow")
 public class followController {
+	@Autowired
+	private BlogBO blogBO;
+	
 	
 	@RequestMapping("/request-view")
-	public String followRequestView() {
+	public String followRequestView(Model model,
+			HttpSession session) {
+		
+		//  로그인 여부 확인
+		Integer userId = (Integer)session.getAttribute("userId");
+		
+		List<CardView> cardViewList = blogBO.generateCardViewList(userId);
+		model.addAttribute("cardViewList", cardViewList);
+		
 		
 		return "follow/request";
 	}
--- src/main/java/com/blog/search/searchController.java
+++ src/main/java/com/blog/search/searchController.java
@@ -1,14 +1,32 @@
 package com.blog.search;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.blog.blog.bo.BlogBO;
+import com.blog.blog.domain.CardView;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 @RequestMapping("/search")
 public class searchController {
+	@Autowired
+	private BlogBO blogBO;
 	
 	@RequestMapping("/user-view")
-	public String searchUserView() {
+	public String searchUserView(Model model,
+			HttpSession session) {
+			//  로그인 여부 확인
+			Integer userId = (Integer)session.getAttribute("userId");
+			
+			List<CardView> cardViewList = blogBO.generateCardViewList(userId);
+			model.addAttribute("cardViewList", cardViewList);
+			
 		
 		return "search/searchUser";
 	}
